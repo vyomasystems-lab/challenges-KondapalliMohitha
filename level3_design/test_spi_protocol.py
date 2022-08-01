76cdfb30a4fc736360df654bea1323a02099fd98
KondapalliMohitha <mohithareddy.k@gmail.com>
2022-08-01 04:04:54 +0000

    updated

---
+++ level3_design/test_spi_protocol.py
@@ -0,0 +1,7 @@
+import random
+import sys
+import cocotb
+from cocotb.decorators import coroutine
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.result import TestFailure
+from cocotb.clock import Clock
